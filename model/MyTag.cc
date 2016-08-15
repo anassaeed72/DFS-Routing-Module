901615aade3bb2c39d0d358f17cd80b43bd3698f
Anas Saeed <anassaeed72@gmail.com>
2016-08-15 12:29:43 -0500

    DFS Workign with Ipv4 Global

---
+++ model/MyTag.cc
@@ -0,0 +1,115 @@
+#include "MyTag.h"
+uint32_t
+MyTag::GetTagItems(void) const{
+  return (uint32_t)(m_items+2);
+}
+void
+MyTag::SetTagItems(int maxItems) {
+  m_items = maxItems;
+}
+TypeId 
+MyTag::GetTypeId (void)
+{
+  static TypeId tid = TypeId ("ns3::MyTag")
+    .SetParent<Tag> ()
+    .AddConstructor<MyTag> ()
+  ;
+  return tid;
+}
+TypeId 
+MyTag::GetInstanceTypeId (void) const
+{
+  return GetTypeId ();
+}
+uint32_t 
+MyTag::GetSerializedSize (void) const
+{
+  // SetTagItems(2);
+  return (uint8_t)((int)GetTagItems() +1);
+}
+void 
+MyTag::Serialize (TagBuffer i) const
+{
+  
+  std::cout <<"Serialize called\n";
+  i.WriteU8((uint8_t)m_simpleValue.size());
+  i.WriteVector (m_simpleValue);
+  std::cout << "Serialize done\n";
+  printTag(this);
+}
+void 
+MyTag::Deserialize (TagBuffer i)
+{
+  std::cout <<"Deserialize called\n";
+  int itemsToRead = (int) i.ReadU8();
+  m_simpleValue = i.ReadVector (itemsToRead);
+  std::cout <<"Deserialize done\n";
+  printTag(this);
+}
+void 
+MyTag::Print (std::ostream &os) const
+{
+  os << "v=";
+  for (auto x : m_simpleValue){
+    os << x << " ";
+  }
+}
+void 
+MyTag::SetSimpleValue (std::vector<uint8_t> value)
+{
+  m_simpleValue = value;
+  SetTagItems(m_simpleValue.size());
+}
+
+std::vector<uint8_t> 
+MyTag::GetSimpleValue (void) const
+{
+  return m_simpleValue;
+}
+void 
+MyTag::SetSimpleValueByIndex(int index, uint8_t value){
+  m_simpleValue[index] = value;
+}
+uint8_t MyTag::GetSimpleValueByIndex(int index) const{
+  return m_simpleValue[index];
+}
+void 
+MyTag::SetParentValueByIndex(int index, uint8_t value){
+  SetSimpleValueByIndex(((index-1)*2)+2,value);
+}
+uint8_t 
+MyTag::GetParentValueByIndex(int index) const{
+  return GetSimpleValueByIndex(((index-1)*2)+2);
+}
+void 
+MyTag::SetCurrValueByIndex( int index, uint8_t value){
+  SetSimpleValueByIndex(((index-1)*2)+3,value);  
+}
+uint8_t 
+MyTag::GetCurrValueByIndex(int index) const{
+  return GetSimpleValueByIndex(((index-1)*2)+3);
+}
+void 
+MyTag::SetInputPort(uint8_t portNumber){
+  SetSimpleValueByIndex(1,portNumber);
+}
+uint8_t
+MyTag::GetInputPort(void) const{
+  return GetSimpleValueByIndex(1);
+}
+void 
+MyTag::printTag(MyTag tag){
+  std::cout << "Printing tag size = "<< tag.GetSimpleValue().size()  << "\nv= ";
+  for( uint8_t x: tag.GetSimpleValue()){
+    std::cout << x << " ";
+  }
+  std::cout <<"\n-----------------------\n";
+}
+void 
+MyTag::SetPacketStart(void) {
+  SetSimpleValueByIndex(0,0x01);
+}
+uint8_t 
+MyTag::GetPacketStart(void) const{
+  return GetSimpleValueByIndex(0);
+}
---
+++ model/MyTag.h
@@ -0,0 +1,149 @@
+// mytag for dfs
+
+#include "ns3/packet.h"
+#include "ns3/log.h"
+#include "ns3/callback.h"
+#include "ns3/ipv4-address.h"
+#include "ns3/ipv4-route.h"
+#include "ns3/node.h"
+#include "ns3/socket.h"
+#include "ns3/net-device.h"
+#include "ns3/uinteger.h"
+#include "ns3/trace-source-accessor.h"
+#include "ns3/object-vector.h"
+#include "ns3/ipv4-header.h"
+#include "ns3/boolean.h"
+#include "ns3/ipv4-routing-table-entry.h"
+#include "ns3/traffic-control-layer.h"
+
+#include "ns3/tag.h"
+#include "ns3/packet.h"
+#include "ns3/tag.h"
+#include "ns3/packet.h"
+#include "ns3/uinteger.h"
+#include "ns3/log.h"
+
+#include <iostream>
+#include <vector>
+using namespace ns3;
+using namespace ns3;
+class MyTag : public Tag
+{
+public:
+  MyTag(){}
+  MyTag(int numberOfSwitches){
+    std::vector<uint8_t> vectorForPacket(numberOfSwitches);
+    for (int i = 0; i < numberOfSwitches*2+2; ++i){
+
+      vectorForPacket.push_back(0x00);
+    }
+    SetSimpleValue(vectorForPacket);
+  }
+  static TypeId GetTypeId (void){
+  static TypeId tid = TypeId ("ns3::MyTag")
+    .SetParent<Tag> ()
+    .AddConstructor<MyTag> ()
+  ;
+  return tid;
+}
+  virtual TypeId GetInstanceTypeId (void) const{
+  return GetTypeId ();
+}
+  virtual uint32_t GetSerializedSize (void) const{
+  // SetTagItems(2);
+  return (uint8_t)((int)GetTagItems() +1);
+}
+  virtual void Serialize (TagBuffer i) const{
+  
+  std::cout <<"Serialize called\n";
+  i.WriteU8((uint8_t)m_simpleValue.size());
+  i.WriteVector (m_simpleValue);
+  std::cout << "Serialize done";
+  printTag();
+}
+  virtual void Deserialize (TagBuffer i){
+  std::cout <<"Deserialize called\n";
+  int itemsToRead = (int) i.ReadU8();
+  m_simpleValue = i.ReadVector (itemsToRead);
+  printTag();
+}
+  virtual void Print (std::ostream &os) const{
+  os << "v=";
+  for (auto x : m_simpleValue){
+
+    os << x << " ";
+  }
+}
+
+  // these are our accessors to our tag structure
+  void SetSimpleValue (std::vector<uint8_t> value){
+  m_simpleValue = value;
+  SetTagItems(m_simpleValue.size());
+}
+  std::vector<uint8_t> GetSimpleValue (void) const{
+  return m_simpleValue;
+}
+  void SetTagItems(int maxItems) {
+  m_items = maxItems;
+}
+  uint32_t GetTagItems (void)const{
+  return (uint32_t)(m_items+2);
+}
+  void SetSimpleValueByIndex(int index, uint8_t value){
+    std::cout << "SetSimpleValueByIndex called\n";
+    m_simpleValue[index] = value;
+    printTag();
+}
+  uint8_t GetSimpleValueByIndex(int index) const{
+  return m_simpleValue[index];
+}
+  void SetPacketStart(void) {
+    std::cout <<"SetPacketStart called\n";
+  SetSimpleValueByIndex(0,0x01);
+}
+  uint8_t GetPacketStart(void) const{
+  return GetSimpleValueByIndex(0);
+}
+  void SetParentValueByIndex(int index, uint8_t value){
+  std::cout << "SetParentValueByIndex i="  << index << " v=" << value <<"\n";
+  SetSimpleValueByIndex(((index)*2)+1,value);
+}
+  uint8_t GetParentValueByIndex(int index) const{
+  return GetSimpleValueByIndex(((index)*2)+1);
+}
+  void SetCurrValueByIndex( int index, uint8_t value){
+    std::cout << "SetCurrValueByIndex i="  << index << " v=" << value <<"\n"; 
+  SetSimpleValueByIndex(((index)*2)+2,value);  
+}
+  uint8_t GetCurrValueByIndex(int index) const{
+  return GetSimpleValueByIndex(((index)*2)+2);
+}
+  void SetInputPort(uint8_t portNumber);
+  uint8_t GetInputPort(void) const;
+  void printTag(MyTag tag){
+  std::cout << "Printing tag size = "<< tag.GetSimpleValue().size()  << "\nv= ";
+  for( uint8_t x: tag.GetSimpleValue()){
+    std::cout << x << " ";
+  }
+  std::cout <<"  ||\n-----------------------\n";
+}
+void printTag() const{
+  std::cout << "Printing tag size = "<< GetSimpleValue().size()  << "\nv= ";
+  for( uint8_t x: GetSimpleValue()){
+     if (x ==0x00)
+    {
+      /* code */
+      std::cout <<"-"; 
+      continue;
+    }
+    std::cout << x << " ";
+  }
+  std::cout <<"  ||\n-----------------------\n";
+}
+
+private:
+  std::vector<uint8_t> m_simpleValue;
+  int m_items;
+};
+
+
